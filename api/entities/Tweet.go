96fa89e55894e977e6998bea6e3624e7e1174aae
Pedro Fagundes <pedrohfagundes2002@gmail.com>
2023-10-10 14:09:45 -0300

    apiRest

---
+++ api/entities/Tweet.go
@@ -0,0 +1,17 @@
+package entities
+
+import (
+  "github.com/pborman/uuid"
+)
+type Tweet struct{
+	ID string `json:"id"`
+	Description string `json:"description"`
+}
+
+func NewTweet() *Tweet{
+	tweet := Tweet{
+		ID: uuid.New(),
+	}
+
+	return &tweet
+}
